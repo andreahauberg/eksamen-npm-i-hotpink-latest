4085a654887f1b751ea1bd5a1e2f8317d015bd6c
michelleenoe <122521774+michelleenoe@users.noreply.github.com>
2024-05-13 09:59:09 +0200

    ok

--- src/components/backend/Tickets.jsx
+++ src/components/backend/Tickets.jsx
@@ -10,6 +10,7 @@ export default function Tickets({ onSelect }) {
       <select value={ticketType} onChange={(e) => setTicketType(e.target.value)}>
         <option value="regular">Regular - 799,-</option>
         <option value="vip">VIP - 1299,-</option>
+        {/* her skal der indsættes (fetches) data fra databasen/lokal json */}
       </select>
       <input type="number" value={quantity} min="1" onChange={(e) => setQuantity(e.target.value)} />
       <button onClick={() => onSelect(ticketType, quantity)}>Select Tickets</button>
