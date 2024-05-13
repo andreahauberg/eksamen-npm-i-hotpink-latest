bf2756f8cee2972e4fe3fc4efe892e4bd350badd
michelleenoe <122521774+michelleenoe@users.noreply.github.com>
2024-05-13 14:25:58 +0200

    ok

---
+++ src/app/api/api.jsx
@@ -0,0 +1,17 @@
+// api.jsx
+const baseURL = process.env.NEXT_PUBLIC_LOCAL_URL;
+
+
+export const fetchAPI = async (endpoint) => {
+  const url = `${baseURL}${endpoint}`;
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return await response.json();
+  } catch (error) {
+    console.error("Error fetching data: ", error);
+    throw error;
+  }
+};
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,22 @@
+"use client";
 import Link from 'next/link';
+import { useEffect } from 'react';
+import { fetchAPI } from './api/api';
 
 export default function Home() {
+  useEffect(() => {
+    const getBands = async () => {
+      try {
+        const bands = await fetchAPI('/bands');
+        console.log('Fetched bands:', bands);
+      } catch (error) {
+        console.error('Failed to fetch bands:', error);
+      }
+    };
+
+    getBands();
+  }, []); 
+
   return (
     <>
       <h1>Foofest</h1>
@@ -8,7 +24,7 @@ export default function Home() {
         <Link href="/booking/">
           <button>Go to Booking</button>
         </Link> 
-        </div>
+      </div>
     </>
   );
 }
