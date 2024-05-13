b72476df16c55b18d039618f4fd1243e1bcc1228
michelleenoe <122521774+michelleenoe@users.noreply.github.com>
2024-05-13 20:12:29 +0200

    ok

--- src/components/backend/Camping.jsx
+++ src/components/backend/Camping.jsx
@@ -1,17 +1,14 @@
-export default function Camping({ onSelect }) {
+export default function Camping({ onCampingOptionSelect }) {
   return (
     <div>
       <h2>Camping Options:</h2>
       <label>
-        <input type="checkbox" onChange={(e) => onSelect('greenCamping', e.target.checked)} />
+        <input type="checkbox" onChange={(e) => onCampingOptionSelect('greenCamping', e.target.checked)} />
         Green Camping Option +249,-
       </label>
+      <input type="checkbox" onChange={(e) => onCampingOptionSelect('twoPersonTent', e.target.checked)} />
       <label>
-        <input type="checkbox" onChange={(e) => onSelect('twoPersonTent', e.target.checked)} />
-        2 Person Tent (including the tent) 299,-
-      </label>
-      <label>
-        <input type="checkbox" onChange={(e) => onSelect('threePersonTent', e.target.checked)} />
+        <input type="checkbox" onChange={(e) => onCampingOptionSelect('threePersonTent', e.target.checked)} />
         3 Person Tent (including the tent) 399,-
       </label>
     </div>
