22b62fc832bacb222fc355d192fda914b1f5e607
andreadamgaard <124664759+andreadamgaard@users.noreply.github.com>
2024-05-14 10:10:18 +0200

    header

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,8 @@
 import "../styles/globals.css";
 import "../styles/reset.css";
 import { lato } from "./fonts";
+import Header from "../components/globals/Header";
+import Footer from "../components/globals/Footer";
 
 export const metadata = {
   title: "Foofest",
@@ -11,9 +13,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className={lato.className}>
       <body>
+        <Header />
         <main>
           <div>{children}</div>
         </main>
+        <Footer />
       </body>
     </html>
   );
