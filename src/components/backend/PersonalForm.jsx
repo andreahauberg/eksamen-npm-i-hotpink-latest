cb0651f1e57fbc42d01761c210ffb11e67cefbd9
Andrea <132267442+andreahauberg@users.noreply.github.com>
2024-05-17 09:32:24 +0200

    personal info required tekster

--- src/components/backend/PersonalForm.jsx
+++ src/components/backend/PersonalForm.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { Field, Label } from "@headlessui/react";
+import { Description, Field, Label } from "@headlessui/react";
 import clsx from "clsx";
 import prices from "../backend/settings.js";
 import { krona_one } from "@/app/fonts.jsx";
@@ -108,25 +108,28 @@ export default function PersonalForm({
             </legend>
             {localPersonalInfo.map((info, index) => (
               <div key={index} className="space-y-4">
-                <h3 className="text-lg font-semibold text-secondaryTextColor">
-                  Ticket {index + 1} ({ticketType})
+                <h3 className="normal-size font-semibold">
+                  Billet {index + 1} ({ticketType})
                 </h3>
 
                 <Field className="space-y-2">
-                  <Label htmlFor={`firstName-${index}`}>First name:</Label>
+                  <Label htmlFor={`firstName-${index}`}>Fornavn:</Label>
                   <input
                     id={`firstName-${index}`}
                     type="text"
                     value={info.firstName}
-                    className="w-full p-2 bg-inputFieldColor text-bgColor rounded-lg focus:outline-none focus:ring-2 focus:ring-accentColor"
-                    aria-label={`First name for ticket ${index + 1}`}
+                    className="peer w-full p-2 bg-inputFieldColor text-bgColor rounded-lg focus:outline-none focus:ring-2 focus:ring-accentColor"
+                    aria-label={`Fornavn for billet ${index + 1}`}
                     required
                     pattern="^[A-Za-z]+$"
-                    title="First name must contain only letters."
+                    title="Fornavn må kun indeholde bogstaver."
                     onChange={(e) =>
                       handleInputChange(index, "firstName", e.target.value)
                     }
                   />
+                  <Description className="xsmall-size hidden peer-focus:block">
+                    Fornavn må kun indeholde bogstaver
+                  </Description>
                 </Field>
 
                 <Field className="space-y-2">
@@ -135,15 +138,18 @@ export default function PersonalForm({
                     id={`lastName-${index}`}
                     type="text"
                     value={info.lastName}
-                    className="w-full p-2 bg-inputFieldColor text-bgColor rounded-lg focus:outline-none focus:ring-2 focus:ring-accentColor"
-                    aria-label={`Last name for ticket ${index + 1}`}
+                    className="peer w-full p-2 bg-inputFieldColor text-bgColor rounded-lg focus:outline-none focus:ring-2 focus:ring-accentColor"
+                    aria-label={`Efternavn for billet ${index + 1}`}
                     required
                     pattern="^[A-Za-z]+$"
-                    title="Last name must contain only letters."
+                    title="Efternavn må kun indeholde bogstaver."
                     onChange={(e) =>
                       handleInputChange(index, "lastName", e.target.value)
                     }
                   />
+                  <Description className=" xsmall-size hidden peer-focus:block">
+                    Efternavn må kun indeholde bogstaver
+                  </Description>
                 </Field>
 
                 <Field className="space-y-2">
@@ -152,16 +158,19 @@ export default function PersonalForm({
                     id={`phoneNumber-${index}`}
                     type="tel"
                     value={info.phoneNumber}
-                    className="w-full p-2 border bg-inputFieldColor text-bgColor rounded-lg focus:outline-none focus:ring-2 focus:ring-accentColor"
+                    className="peer w-full p-2 border bg-inputFieldColor text-bgColor rounded-lg focus:outline-none focus:ring-2 focus:ring-accentColor"
                     aria-label={`Phone number for ticket ${index + 1}`}
                     required
                     pattern="^\+\d+$"
-                    title="Phone number must start with a + and contain only numbers."
+                    title="Telefonnummeret skal starte med et + og kun indeholde tal."
                     onKeyPress={handlePhoneKeyPress}
                     onChange={(e) =>
                       handleInputChange(index, "phoneNumber", e.target.value)
                     }
                   />
+                  <Description className=" xsmall-size hidden peer-focus:block">
+                    Telefonnummeret skal starte med et + og kun indeholde tal
+                  </Description>
                 </Field>
 
                 <Field className="space-y-2">
@@ -170,8 +179,8 @@ export default function PersonalForm({
                     id={`dateOfBirth-${index}`}
                     type="date"
                     value={info.dateOfBirth}
-                    className="w-full p-2 bg-inputFieldColor text-bgColor rounded-lg focus:outline-none focus:ring-2 focus:ring-accentColor"
-                    aria-label={`Date of birth for ticket ${index + 1}`}
+                    className="peer w-full p-2 bg-inputFieldColor text-bgColor rounded-lg focus:outline-none focus:ring-2 focus:ring-accentColor"
+                    aria-label={`Fødelsdato for billet ${index + 1}`}
                     required
                     min="1923-01-01"
                     max={new Date().toISOString().split("T")[0]}
@@ -180,11 +189,14 @@ export default function PersonalForm({
                         handleInputChange(index, "dateOfBirth", e.target.value);
                       } else {
                         alert(
-                          "Date of birth must be a valid date not in the future and less than 100 years ago."
+                          "Fødselsdatoen skal være en gyldig dato, som ikke ligger i fremtiden og er mindre end 100 år gammel."
                         );
                       }
                     }}
                   />
+                  <Description className="xsmall-size hidden peer-focus:block">
+                    Fødselsdatoen skal være en gyldig dato
+                  </Description>
                 </Field>
 
                 <Field className="space-y-2">
@@ -193,38 +205,23 @@ export default function PersonalForm({
                     id={`email-${index}`}
                     type="email"
                     value={info.email}
-                    className="w-full p-2 bg-inputFieldColor text-bgColor rounded-lg focus:outline-none focus:ring-2 focus:ring-accentColor"
-                    aria-label={`Email for ticket ${index + 1}`}
+                    className="peer w-full p-2 bg-inputFieldColor text-bgColor rounded-lg focus:outline-none focus:ring-2 focus:ring-accentColor"
+                    aria-label={`Email for billet ${index + 1}`}
                     required
                     pattern="^[^\s@]+@[^\s@]+\.[^\s@]+$"
-                    title="Email must be a valid email address."
+                    title="Email skal være en gyldig emailadresse."
                     onChange={(e) =>
                       handleInputChange(index, "email", e.target.value)
                     }
                   />
+                  <Description className="xsmall-size hidden peer-focus:block">
+                    {" "}
+                    Email skal være en gyldig emailadresse
+                  </Description>
                 </Field>
               </div>
             ))}
           </fieldset>
-
-          {/* <fieldset className="space-y-6 mt-8">
-            <legend className="text-primaryTextColor">Kurvens indhold</legend>
-            <p>
-              {ticketType.charAt(0).toUpperCase() + ticketType.slice(1)} ticket
-              x {ticketQuantity}
-            </p>
-            {campingOptions.selectedArea && (
-              <p>Camping area: {campingOptions.selectedArea}</p>
-            )}
-            {campingOptions.twoPersonTent > 0 && (
-              <p>2 person Tent x {campingOptions.twoPersonTent}</p>
-            )}
-            {campingOptions.threePersonTent > 0 && (
-              <p>3 person Tent x {campingOptions.threePersonTent}</p>
-            )}
-            <p>Booking fee: {prices.fee} kr.</p>
-            <h3>Total: {calculateTotalPrice()} kr.</h3>
-          </fieldset> */}
 
           <div className="flex justify-between mt-8">
             <button
