ba6157e7b0901e0f0f4bdbf0357fe2e171f7bf72
michelleenoe <122521774+michelleenoe@users.noreply.github.com>
2024-05-22 11:59:40 +0200

    Optimeret progressbar!

--- src/components/backend/ProgressBar.jsx
+++ src/components/backend/ProgressBar.jsx
@@ -1,51 +1,19 @@
 import styles from "./ProgressBar.module.css";
 
 export default function ProgressBar({ currentStep }) {
+  const steps = [1, 2, 3, 4, 5];
+  
   return (
     <div className="grid grid-cols-gridContent">
       <div className="pt-6 col-start-3 flex items-center justify-center">
         <section className={styles.progressbar_container}>
           <ul className={styles.steps_container}>
-            <li
-              className={`${styles.step_item} ${
-                currentStep === 1 ? styles.current_item : ""
-              }`}
-            >
-              <span className={styles.progress_count}>1</span>
-              <span className={styles.step_label}></span>
-            </li>
-            <li
-              className={`${styles.step_item} ${
-                currentStep === 2 ? styles.current_item : ""
-              }`}
-            >
-              <span className={styles.progress_count}>2</span>
-              <span className={styles.step_label}></span>
-            </li>
-            <li
-              className={`${styles.step_item} ${
-                currentStep === 3 ? styles.current_item : ""
-              }`}
-            >
-              <span className={styles.progress_count}>3</span>
-              <span className={styles.step_label}></span>
-            </li>
-            <li
-              className={`${styles.step_item} ${
-                currentStep === 4 ? styles.current_item : ""
-              }`}
-            >
-              <span className={styles.progress_count}>4</span>
-              <span className={styles.step_label}></span>
-            </li>
-            <li
-              className={`${styles.step_item} ${
-                currentStep === 5 ? styles.current_item : ""
-              }`}
-            >
-              <span className={styles.progress_count}>5</span>
-              <span className={styles.step_label}></span>
-            </li>
+            {steps.map(step => (
+              <li key={step} className={`${styles.step_item} ${currentStep === step ? styles.current_item : ""}`}>
+                <span className={styles.progress_count}>{step}</span>
+                <span className={styles.step_label}></span>
+              </li>
+            ))}
           </ul>
         </section>
       </div>
