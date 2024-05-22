d9af53252a29746862170edfd0f9c2055d70e518
Andrea <132267442+andreahauberg@users.noreply.github.com>
2024-05-22 19:53:35 +0200

    progressbar

--- src/app/booking/page.jsx
+++ src/app/booking/page.jsx
@@ -1,85 +1,81 @@
 "use client";
-import { useState } from 'react';
-import TicketsForm from '../../components/backend/Tickets';
-import Camping from '../../components/backend/Camping';
-import PersonalForm from '../../components/backend/PersonalForm';
-import SummaryPage from '../../components/backend/Summary';
-import PaymentPage from '../../components/backend/Payment';
-import ConfirmationPage from '../../components/backend/Confirmation';
-import ProgressBar from '../../components/backend/ProgressBar';
+import { useState } from "react";
+import TicketsForm from "../../components/backend/Tickets";
+import Camping from "../../components/backend/Camping";
+import PersonalForm from "../../components/backend/PersonalForm";
+import SummaryPage from "../../components/backend/Summary";
+import PaymentPage from "../../components/backend/Payment";
+import ConfirmationPage from "../../components/backend/Confirmation";
+import ProgressBar from "../../components/backend/ProgressBar";
 
 export default function BookingPage() {
-    const [step, setStep] = useState(1);
-    const [bookingData, setBookingData] = useState({
-        ticketType: 'regular', // Default value
-        ticketQuantity: 1,
-        camping: {},
-        personalInfo: [],
-        totalPrice: 0,
-        orderId: ''
-    });
+  const [step, setStep] = useState(1);
+  const [bookingData, setBookingData] = useState({
+    ticketType: "regular", // Default value
+    ticketQuantity: 1,
+    camping: {},
+    personalInfo: [],
+    totalPrice: 0,
+    orderId: "",
+  });
 
-    const nextStep = () => setStep(step + 1);
-    const prevStep = () => setStep(step - 1);
+  const nextStep = () => setStep(step + 1);
+  const prevStep = () => setStep(step - 1);
 
-    const handleBookingChange = (data) => {
+  const handleBookingChange = (data) => {
     console.log(data);
-        setBookingData(prevData => ({ ...prevData, ...data }));
-    };
+    setBookingData((prevData) => ({ ...prevData, ...data }));
+  };
 
-    return (
-        <div>
-            <ProgressBar currentStep={step} />
-            {step === 1 && (
-                <TicketsForm
-                    ticketType={bookingData.ticketType}
-                    ticketQuantity={bookingData.ticketQuantity}
-                    onClick={handleBookingChange}
-                    onNext={nextStep}
-                />
-            )}
-            {step === 2 && (
-                <Camping
-                    ticketQuantity={bookingData.ticketQuantity}
-                    ticketType={bookingData.ticketType}
-                    campingOptions={bookingData.camping}
-                    onClick={handleBookingChange}
-                    //updateBasket
-                    onNext={nextStep}
-                    onBack={prevStep}
-                />
-            )}
-            {step === 3 && (
-                <PersonalForm
-                    personalInfo={bookingData.personalInfo}
-                    ticketQuantity={bookingData.ticketQuantity}
-                    ticketType={bookingData.ticketType}
-                    campingOptions={bookingData.camping}
-                    totalPrice={bookingData.totalPrice}
-                    onClick={handleBookingChange}
-                    onNext={nextStep}
-                    onBack={prevStep}
-                />
-            )}
-            {step === 4 && (
-                <SummaryPage
-                    bookingData={bookingData}
-                    onBack={prevStep}
-                    onNext={nextStep}
-                />
-            )}
-            {step === 5 && (
-                <PaymentPage
-                    bookingData={bookingData}
-                    onBack={prevStep}
-                    onNext={nextStep}
-                />
-            )}
-            {step === 6 && (
-                <ConfirmationPage
-                    bookingData={bookingData}
-                />
-            )}
-        </div>
-    );
+  return (
+    <div>
+      <ProgressBar currentStep={step} />
+      {step === 1 && (
+        <TicketsForm
+          ticketType={bookingData.ticketType}
+          ticketQuantity={bookingData.ticketQuantity}
+          onClick={handleBookingChange}
+          onNext={nextStep}
+        />
+      )}
+      {step === 2 && (
+        <Camping
+          ticketQuantity={bookingData.ticketQuantity}
+          ticketType={bookingData.ticketType}
+          campingOptions={bookingData.camping}
+          onClick={handleBookingChange}
+          //updateBasket
+          onNext={nextStep}
+          onBack={prevStep}
+        />
+      )}
+      {step === 3 && (
+        <PersonalForm
+          personalInfo={bookingData.personalInfo}
+          ticketQuantity={bookingData.ticketQuantity}
+          ticketType={bookingData.ticketType}
+          campingOptions={bookingData.camping}
+          totalPrice={bookingData.totalPrice}
+          onClick={handleBookingChange}
+          onNext={nextStep}
+          onBack={prevStep}
+        />
+      )}
+      {step === 4 && (
+        <SummaryPage
+          bookingData={bookingData}
+          onBack={prevStep}
+          onNext={nextStep}
+        />
+      )}
+      {step === 5 && (
+        <PaymentPage
+          bookingData={bookingData}
+          onBack={prevStep}
+          onNext={nextStep}
+        />
+      )}
+      {step === 6 && <ConfirmationPage bookingData={bookingData} />}
+    </div>
+  );
 }
