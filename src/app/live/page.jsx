a6f77746896b5d22c93a5a67ac991f42e2307911
michelleenoe <122521774+michelleenoe@users.noreply.github.com>
2024-05-24 12:32:30 +0200

    added outline

--- src/app/live/page.jsx
+++ src/app/live/page.jsx
@@ -91,74 +91,73 @@ export default function Schedule() {
 
   return (
     <>
-      <div className="mb-32">
+      <article className="mb-32">
         <div className={`${krona_one.className} headliner text-center`}>
           <h1>Spiller nu</h1>
         </div>
         <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 px-6 py-5">
           {Object.keys(groupedByScene).map((scene) => (
-            <div key={scene} className="overflow-hidden bg-secondaryBgColor p-8 rounded-lg shadow-md shadow-primaryColor relative">
-              <div className="large-size mb-1 text-primaryTextColor flex justify-between items-center">
-                <h2 className="text-3xl p-4">{scene}</h2>
+            <div key={scene}>
+              <div className=" text-3xl items-center flex mb-4">
+                <h2 className="text-3xl flex">{scene} - </h2>
                 {groupedByScene[scene].length > 0 && (
-                  <span className="text-sm text-input-bg-color">
-                    {groupedByScene[scene][0].start}
+                  <span className="text-3xl">
+                      {groupedByScene[scene][0].start}
                   </span>
                 )}
               </div>
-              {groupedByScene[scene].length > 0 ? (
-                <ul className="divide-y divide-gray-700">
-                  {groupedByScene[scene].map((act) => (
-                    <li
-                      key={`${act.act}-${act.scene}`}
-                      className="flex items-center p-4"
-                    >
-                      {act.act && (
-                        <Link
-                          href={act.band?.slug || "#"}
-                          prefetch={false}
-                          className="flex items-center space-x-4 w-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-accentColor"
-                          tabIndex={0}
-                        >
-                          <div className="flex-shrink-0">
-                            {act.band && (
-                              <div className="relative h-24 w-24 md:w-32 md:h-32">
-                                <Image
-                                  src={
-                                    act.band.logo.includes("https")
-                                      ? act.band.logo
-                                      : `/logos/${act.band.logo}`
-                                  }
-                                  fill
-                                  alt="Billede af band der spiller"
-                                  className="rounded-full object-cover"
-                                />
-                              </div>
-                            )}
+              <div className="overflow-hidden bg-secondaryBgColor p-8 rounded-lg shadow-md shadow-primaryColor relative">
+                {groupedByScene[scene].length > 0 ? (
+                  <ul className="divide-y divide-gray-700">
+                    {groupedByScene[scene].map((act) => (
+                      <li
+                        key={`${act.act}-${act.scene}`}
+                        className="flex items-center p-4"
+                      >
+                        {act.act && (
+                          <Link
+                            href={act.band?.slug || "#"}
+                            prefetch={false}
+                            className="flex items-center space-x-4 w-full focus:outline-none focus:ring-2 focus:ring-offset-1 focus:ring-accentColor"
+                            tabIndex={0}
+                          >
+                            <div className="flex-shrink-0">
+                              {act.band && (
+                                <div className="relative h-24 w-24 md:w-32 md:h-32">
+                                  <Image
+                                    src={
+                                      act.band.logo.includes("https")
+                                        ? act.band.logo
+                                        : `/logos/${act.band.logo}`
+                                    }
+                                    fill
+                                    alt="Billede af band der spiller"
+                                    className="rounded-full object-cover"
+                                  />
+                                </div>
+                              )}
+                            </div>
+                            <div className="flex-1 min-w-0">
+                              <p className={`${krona_one.className} text-lg`}>{act.act}</p>
+                            </div>
+                          </Link>
+                        )}
+                        {act.act === "break" && (
+                          <div className="relative h-24 w-24 md:w-32 md:h-32 flex items-center justify-center">
+                            <span className={`${krona_one.className} text-lg`}>Break</span>
                           </div>
-                          <div className="flex-1 min-w-0">
-                            <p className={`${krona_one.className} text-lg`}>{act.act}</p>
-                          </div>
-                          <div className="text-right">
-                            <p className="text-sm text-input-bg-color">{act.start}</p>
-                          </div>
-                        </Link>
-                      )}
-                      {act.act === "break" && (
-                        <div className="relative h-24 w-24 md:w-32 md:h-32 flex items-center justify-center">
-                          <span className={`${krona_one.className} text-lg`}>Break</span>
-                        </div>
-                      )}
-                    </li>
-                  ))}
-                </ul>
-              ) : (
-                <p className="p-4">På nuværende tidspunkt, er der ingen der spiller.</p>
-              )}
+                        )}
+                      </li>
+                    ))}
+                  </ul>
+                ) : (
+                  <p className="p-4">På nuværende tidspunkt, er der ingen der spiller.</p>
+                )}
+              </div>
             </div>
           ))}
         </section>
-      </div>
+      </article>
     </>
   );
 }
