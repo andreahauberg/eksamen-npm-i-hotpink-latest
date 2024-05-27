a3130add2236017787e1bf850e4c0244967bb7e6
Andrea <132267442+andreahauberg@users.noreply.github.com>
2024-05-27 14:39:17 +0200

    band semantik og tilgængelighed

--- src/app/[slug]/page.jsx
+++ src/app/[slug]/page.jsx
@@ -1,9 +1,3 @@
-import { fetchAPI } from "../../app/api/api.js";
-import Image from "next/image.js";
-import { krona_one } from "@/app/fonts";
-import BandBio from "@/components/frondend/BandBio.jsx";
-import { notFound } from "next/navigation.js";
-
 // export async function generateMetadata({ params }) {
 //   const { slug } = params;
 //   const fetchData = await fetchAPI("/bands");
@@ -21,6 +15,12 @@ import { notFound } from "next/navigation.js";
 //     slug: band.slug,
 //   }));
 // }
+
+import { fetchAPI } from "../../app/api/api.js";
+import Image from "next/image.js";
+import { krona_one } from "@/app/fonts";
+import BandBio from "@/components/frondend/BandBio.jsx";
+import { notFound } from "next/navigation.js";
 
 export default async function Band({ params }) {
   const { slug } = params;
@@ -47,47 +47,85 @@ export default async function Band({ params }) {
   }
 
   return (
-    <>
-      <section className="grid grid-cols-gridContent">
-        <div className={`${krona_one.className} bandheader pb-6 md:pb-0 text-center col-start-2 col-end-5 pt-5`}>
-          <h1>{band.name}</h1>
-        </div>
-        <div className="col-start-2 col-end-5">
-          <div className="grid md:grid-cols-2">
-            <div className="flex flex-col order-last	md:order-first md:p-5">
-              <div className="flex gap-2 justify-center md:justify-start items-center py-5 md:pt-0 md:pb-11">
-                <div className={`${krona_one.className} uppercase bg-primaryColor small-size text-center text-secondaryTextColor rounded-lg w-28 lg:w-32 lg:px-5 py-1`}>Genre:</div>
-                <div></div>
-                <p className={`${krona_one.className} small-size`}>{band.genre}</p>
+    <article className="grid grid-cols-gridContent">
+      <h1
+        className={`${krona_one.className} bandheader pb-6 md:pb-0 text-center col-start-2 col-end-5 pt-5`}
+      >
+        {band.name}
+      </h1>
+      <section className="col-start-2 col-end-5">
+        <div className="grid md:grid-cols-2">
+          <div className="flex flex-col order-last md:order-first md:p-5">
+            <div className="flex gap-4 justify-center md:justify-start items-center py-5 md:pt-0 md:pb-11">
+              <div
+                className={`${krona_one.className} uppercase bg-primaryColor small-size text-center text-secondaryTextColor rounded-lg w-28 lg:w-32 lg:px-5 py-1`}
+                aria-label="Genre"
+              >
+                Genre:
               </div>
-
-              <div className="pb-6">
-                <BandBio bio={band.bio} />
-              </div>
-              <div className="py-5">
-                <div className="rounded-lg overflow-hidden">
-                  <iframe title="playliste" src="https://open.spotify.com/embed/artist/08GQAI4eElDnROBrJRGE0X?utm_source=generator" width="100%" height="352" frameBorder="0" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy" className="w-full h-88"></iframe>
-                </div>
+              <p className={`${krona_one.className} small-size`}>
+                {band.genre}
+              </p>
+            </div>
+            <div className="pb-6">
+              <BandBio bio={band.bio} />
+            </div>
+            <div className="py-5">
+              <div className="rounded-lg overflow-hidden">
+                <iframe
+                  title={`Spotify playlist for ${band.name}`}
+                  src="https://open.spotify.com/embed/artist/08GQAI4eElDnROBrJRGE0X?utm_source=generator"
+                  width="100%"
+                  height="352"
+                  frameBorder="0"
+                  allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+                  loading="lazy"
+                  className="w-full h-88"
+                ></iframe>
               </div>
             </div>
-            <div className="md:p-5">
-              {bandSchedule.length > 0 &&
-                bandSchedule.map((schedule, index) => (
-                  <div key={index} className="flex justify-between items-center text-center pb-4 md:pb-2">
+          </div>
+          <div className="md:p-5">
+            {bandSchedule.length > 0 && (
+              <div role="region" aria-labelledby="schedule-heading">
+                <h2 id="schedule-heading" className="sr-only">
+                  Band Schedule
+                </h2>
+                {bandSchedule.map((schedule, index) => (
+                  <div
+                    key={index}
+                    className="flex justify-between items-center text-center pb-4 md:pb-2"
+                  >
                     <div className="uppercase bg-labelColor text-secondaryTextColor rounded-lg w-40 md:w-36 lg:w-48 lg:px-5 py-3">
-                      {schedule.day.charAt(0).toUpperCase() + schedule.day.slice(1)} {schedule.start}
+                      {schedule.day.charAt(0).toUpperCase() +
+                        schedule.day.slice(1)}{" "}
+                      {schedule.start}
                     </div>
-                    <div className="uppercase bg-labelColor text-secondaryTextColor rounded-lg w-40 md:w-36  lg:w-48 lg:px-5 py-3">{schedule.stage}</div>
+                    <div className="uppercase bg-labelColor text-secondaryTextColor rounded-lg w-40 md:w-36 lg:w-48 lg:px-5 py-3">
+                      {schedule.stage}
+                    </div>
                   </div>
                 ))}
-              <figure className=" md:py-5">
-                <Image src={band.logo.includes("https") ? band.logo : `/logos/${band.logo}`} width={400} height={400} alt="Picture of Artist" />
-                <p className=" text-xs text-right px-2">{band.logoCredits}</p>
-              </figure>
-            </div>
+              </div>
+            )}
+            <figure className="md:py-5">
+              <Image
+                src={
+                  band.logo.includes("https")
+                    ? band.logo
+                    : `/logos/${band.logo}`
+                }
+                width={400}
+                height={400}
+                alt={`Logo of ${band.name}`}
+              />
+              <figcaption className="text-xs text-right px-2">
+                {band.logoCredits}
+              </figcaption>
+            </figure>
           </div>
         </div>
       </section>
-    </>
+    </article>
   );
 }
