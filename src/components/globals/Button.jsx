0d58d5d2b038679e155875e37c89a61be4e2dd68
Andrea <132267442+andreahauberg@users.noreply.github.com>
2024-06-06 21:15:11 +0200

    button noter

--- src/components/globals/Button.jsx
+++ src/components/globals/Button.jsx
@@ -1,4 +1,5 @@
 const Button = ({ title }) => {
+  // funktionel komponent defineret med en arrow function, som modtager title som prop
   return (
     <>
       <div className=" bg-bgColor rounded-lg border-2 border-inputFieldColor text-secondaryColor transition-colors duration-100 ease-in-out hover:bg-secondaryColor hover:text-bgColor hover:border-bgColor px-5 py-3 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-accentColor">
