b65e3678db80c5c73f6a283c4761ea3a35435020
Andrea <132267442+andreahauberg@users.noreply.github.com>
2024-06-06 21:11:56 +0200

    cart summary noter

--- src/components/backend/CartSummary.jsx
+++ src/components/backend/CartSummary.jsx
@@ -7,28 +7,36 @@ import { ShoppingCartIcon } from "@heroicons/react/24/solid";
 import prices from "../backend/settings.js";
 
 function calculateTotalPrice(ticketQuantity, ticketType, campingOptions) {
+  // params
   const ticketPrice =
     ticketQuantity * (ticketType === "regular" ? prices.regular : prices.vip);
   const addOnPrice =
     (campingOptions.greenCamping ? prices.greenCamping : 0) +
+    // vis greenCamping er valgt (true), tilføjes prices.greenCamping
     campingOptions.twoPersonTent * prices.TwoPersonsTent +
+    // Antallet af 2-personers telte multipliceres med prisen pr. telt
     campingOptions.threePersonTent * prices.ThreePersonsTent;
+  // Antallet af 3-personers telte multipliceres med prisen pr. telt
   return ticketPrice + addOnPrice + prices.fee;
-}
+} // beregner sum af de samlede omkostninger
 
 export default function CartSummary({
   ticketType,
   ticketQuantity,
   campingOptions,
 }) {
+  // modtager de tre props
   const [isOpen, setOpen] = useState(false);
+  // isOpen styrer om kurvens indhold vises eller ej.  setOpen opdatere isOpen
 
   const TicketSVG = ticketType === "regular" ? RegularTicketSVG : VIPTicketSVG;
+  // ternary operator bruges til at vise biltten baseret på typen
 
   return (
     <>
       <button
         onClick={() => setOpen(!isOpen)}
+        // når knappen klikkes skifter isOpen mellem true og false
         className="fixed bottom-4 right-4 bg-primaryColor text-white p-3 rounded-full shadow-lg focus:outline-none focus:ring-2 focus:ring-offset-1 focus:ring-accentColor"
         aria-expanded={isOpen}
         aria-controls="cart-summary"
